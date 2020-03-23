752e9a17507f539533627908bacec2dba7102b25
SylvainCatillon <sylvain.catillon@gmail.com>
2020-03-23 20:57:13 +0100

    Fixed legal.html documented init_products_db

--- substitut_search/management/commands/init_products_db.py
+++ substitut_search/management/commands/init_products_db.py
@@ -4,12 +4,20 @@ from substitut_search.utils.fill_db import FillDB
 from substitut_search.models import Product
 
 class Command(BaseCommand):
+    """Add the command to init the database by dowloading
+    the products on OpenFoodFacts.org"""
     help = 'Init the database by dowloading the products on OpenFoodFacts.org'
 
     def add_arguments(self, parser):
+        """Add a positional argument
+        n_products: the number of products to download"""
         parser.add_argument('n_products', type=int, default=1000)
 
     def handle(self, *args, **options):
+        """-Delete all the products from the database.
+        -Diplay the number of products deleted.
+        -Download and insert the products in the database.
+        -Display the number of products inserted."""
         old_count = Product.objects.count()
         Product.objects.all().delete()
         count = Product.objects.count()
