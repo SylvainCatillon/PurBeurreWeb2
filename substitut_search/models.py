655164713d3cfaca66990cbfa4b975510c9aa022
SylvainCatillon <sylvain.catillon@gmail.com>
2020-03-26 21:36:37 +0100

    Added favicon PEP8 of fill_db.py

--- substitut_search/models.py
+++ substitut_search/models.py
@@ -4,7 +4,8 @@ from django.contrib.postgres.fields import ArrayField
 class Product(models.Model):
     """A product downloaded from OpenFoodFacts"""
     ns_choices = [("a", "A"), ("b", "B"), ("c", "C"), ("d", "D"), ("e", "E")]
-    nutriscore = models.CharField(max_length=1, choices=ns_choices, db_index=True)
+    nutriscore = models.CharField(
+        max_length=1, choices=ns_choices, db_index=True)
     categories = ArrayField(models.CharField(max_length=100))
     name = models.CharField(max_length=200, unique=True)
     image = models.URLField()
@@ -16,7 +17,8 @@ class Product(models.Model):
 
 class Favory(models.Model):
     """Relation table between Product and User"""
-    user_profile = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
+    user_profile = models.ForeignKey(
+        'accounts.Profile', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     saved_at = models.DateTimeField(auto_now_add=True)
 
--- substitut_search/utils/fill_db.py
+++ substitut_search/utils/fill_db.py
@@ -6,15 +6,19 @@ from ..models import Product
 
 
 def trad(word):
+    """Translate the nutriments information in french"""
     initial_words = [
         'saturated-fat', 'fat', 'sugars', 'salt',
         'low', 'moderate', 'high']
     trad_words = [
-        'Acides gras saturés', 'Matières grasses','Sucres', 'Sel',
+        'Acides gras saturés', 'Matières grasses', 'Sucres', 'Sel',
         'faible', 'modérée', 'élevée']
     return trad_words[initial_words.index(word)]
 
+
 def get_nutrients(product):
+    """Get the nutrients from an OpenFoodFact downloaded product,
+    by getting the level and the quantity per 100g"""
     result = []
     for nutrient in ['fat', 'saturated-fat', 'sugars', 'salt']:
         try:
@@ -27,15 +31,17 @@ def get_nutrients(product):
         result.append(string)
     return result
 
+
 class FillDB:
     """Use this class to download products from fr.openfoodfacts.org
     and fill the database"""
-    NB_PRODUCTS = 2223
 
     def __init__(self, nb_products=1000):
         self.nb_products = nb_products
 
     def dl_page(self, nb, page):
+        """Download one page of products from OpenFoodfacts.org.
+        Takes as args the page size as 'nb' and the page number as 'page'"""
         payload = {
             "sort_by": "unique_scans_n",
             "action": "process",
@@ -49,10 +55,15 @@ class FillDB:
         return raw_result.json()["products"]
 
     def dl_products(self):
-        # while nb_products > 1000: req 1000 nb-=1000 page+=1
+        """Download the requested number of products from OpenFoodFacts.
+        As the maximum page size is 1000, the function may need to launch
+        the sub-function 'dl-page' several times"""
         products_list = []
         nb_products = self.nb_products
         page = 1
+        #  The OpenFoodFacts maximum page size is 1000
+        #  The loop will dl the products 1000 by 1000 while incrementing the
+        #  page number, until the number of products left to dl is under 1000
         while nb_products > 1000:
             products_list += self.dl_page(1000, page)
             page += 1
@@ -61,25 +72,21 @@ class FillDB:
         return products_list
 
     def insert_products(self):
+        """Launch 'dl_products' to the products from OpenFoodfacts.org
+        then insert the products in the database"""
         products_list = self.dl_products()
         for product in products_list:
+            #  if the product doesn't contain the right info, go to the next
             try:
                 nutriscore = product["nutrition_grade_fr"].lower()
-                categories = product["categories_tags"] # filtrer les cat qui commencent pas par en:?
-                # if categories == []: raise KeyError
+                categories = product["categories_tags"]
                 name = product["product_name"].title()
-                """# If there is brands indicated for the product, the first of
-                    # them will be used to complement the name of the product
-                    brands = product.get("brands")
-                    if brands:
-                        name += " " + brands.split(",")[0]"""
                 link = product["url"]
                 image = product["image_front_small_url"]
             except KeyError as error:
-                #print(error)
-                #print("The product doesn't contain the needed informations")
                 continue
             nutrient_levels = get_nutrients(product)
+            #  if the product can't be inserted, go to the next
             try:
                 with transaction.atomic():
                     Product.objects.create(
@@ -88,6 +95,4 @@ class FillDB:
                         name=name, link=link, image=image,
                         nutrient_levels=nutrient_levels)
             except (IntegrityError, DataError) as error:
-                #print(error)
                 continue
-        #print(str(Product.objects.count())+" products in the database")
